c59d5d5b489cc1118b98467339881d7c48ed5523
Phillip Sloan <phillipsloan24@gmail.com>
2019-03-06 11:46:07 -0500

    Update

--- open_robinhood_stocks.py
+++ open_robinhood_stocks.py
@@ -1,7 +1,6 @@
 import webbrowser as browser
-import robin_stocks as robinhood
+from get_symbols import getRobinhoodSymbols
 
-login = robinhood.login('phillipsloan24@gmail.com','54bR&Srkm7EU')
-my_stocks = robinhood.build_holdings()
-for ticker in my_stocks.keys():
+my_stocks = getRobinhoodSymbols()
+for ticker in my_stocks:
     browser.open('https://finance.yahoo.com/chart/' + ticker)
