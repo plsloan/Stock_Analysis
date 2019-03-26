f615e1159860d153433d3d713a0e5acf92ba8d86
Phillip Sloan <phillipsloan24@gmail.com>
2019-03-26 11:47:47 -0400

    Add option to do all .csv files

--- update_prices.py
+++ update_prices.py
@@ -3,21 +3,23 @@ from requests import get
 from bs4 import BeautifulSoup
 
 def main():
-    filename = input("Enter date (YYYY-MM-DD): ")
-    filename = 'Data/MACD_Crossover/' + filename + '.csv'
-    df = pandas.read_csv(filename)
-    tickers = df['Symbol']
-
-    updated_prices = []
-    for ticker in tickers:
-        try:
-            updated_prices.append(get_ticker_price(ticker))
-        except:
-            updated_prices.append(-1)
-
-    df['Updated_Price'] = updated_prices
-    df.to_csv(filename)
-
+    response = input('all? ')
+    if (response[0] == 'n'):
+        filename = input("Enter date (YYYY-MM-DD): ")
+        filename = 'Data/MACD_Crossover/' + filename + '.csv'
+        df = pandas.read_csv(filename)
+        tickers = df['Symbol']
+        updated_prices = []
+        for ticker in tickers:
+            try:
+                updated_prices.append(get_ticker_price(ticker))
+            except:
+                updated_prices.append(-1)
+        df['Updated_Price'] = updated_prices
+        df.to_csv(filename)
+    else:
+        pass
+        
 def get_ticker_price(ticker):
     url = 'https://finance.yahoo.com/quote/' + ticker
     html = BeautifulSoup(get(url).content, features="lxml")
