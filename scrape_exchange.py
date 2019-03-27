63af97f317b34edd2d8ec45a30b897b360ec49b0
Phillip Sloan <phillipsloan24@gmail.com>
2019-03-27 10:56:53 -0400

    Add both functionality

--- scrape_exchange.py
+++ scrape_exchange.py
@@ -26,19 +26,32 @@ def main():
             os.makedirs('Data/NYSE/')
         scrape_historical_data(tickers, response)
     elif response.lower()[0] == 'b':
-        pass
+        try:
+            tickers = get_nasdaq_tickers()
+            if not os.path.exists('Data/NASDAQ/'):
+                os.makedirs('Data/NASDAQ/')
+            scrape_historical_data(tickers, 'nasdaq')
+
+            tickers = get_nyse_tickers()
+            if not os.path.exists('Data/NYSE/'):
+                os.makedirs('Data/NYSE/')
+            scrape_historical_data(tickers, 'nyse')
+        except:
+            print("Couldn't find file with tickers...")
+            return -1
+        
     else:
         print('Something was misspelled...')
 
-def scrape_historical_data(tickers, res):
+def scrape_historical_data(tickers, exchange):
     now = datetime.now()
     today = str(now.month) + '/' + str(now.day) + '/' + str(now.year)
     for ticker in tickers:
         url = 'http://quotes.wsj.com/' + ticker + '/historical-prices/download?MOD_VIEW=page&num_rows=6299.041666666667&range_days=6299.041666666667&startDate=01/01/2010&endDate=' + today
         content = get(url).text
-        if res[:2] == 'na':
+        if exchange[:2] == 'na':
             path = 'Data/NASDAQ/'
-        else:
+        elif exchange[:2] == 'ny':
             path = 'Data/NYSE/'
         f = open(path + ticker + '.csv','w')
         f.write(content)
