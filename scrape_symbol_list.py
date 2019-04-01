590b4d54b1fecae6f9ee06738c7f854ede5614c9
Unknown <phillipsloan24@gmail.com>
2019-03-31 22:30:20 -0400

    working scrape of symbols

--- scrape_symbol_list.py
+++ scrape_symbol_list.py
@@ -1,9 +1,18 @@
+import pandas
+import shutil
+
 from datetime import datetime
-import os
 from requests import get 
 from progressbar_mine import progress_bar_mine
 
+import os
+from os import listdir
+from os.path import isfile, join
+
+from update_crossover_prices import get_ticker_price
+
 def main():
+    # get 
     tickers = input('Enter list of symbols (separated by space): ').split(' ')
     progress_bar = progress_bar_mine(len(tickers))
     progress_bar.start()
@@ -11,13 +20,14 @@ def main():
         scrape_symbol(tickers[i])
         progress_bar.update(i)
     progress_bar.finish()
+    compile_file(tickers)
 
 def scrape_symbol(ticker):
-    date = datetime.now().strftime('%Y-%m-%d')
-    path = 'Data/Watchlist/' + date + '/'
+    path = 'Data/Watchlist/Temp/'
     if not os.path.exists(path):
         os.makedirs(path)
     failed_pull = []
+    date = datetime.now().strftime('%Y-%m-%d')
     url = 'http://quotes.wsj.com/' + ticker + '/historical-prices/download?MOD_VIEW=page&num_rows=6299.041666666667&range_days=6299.041666666667&startDate=01/01/2010&endDate=' + date
     content = get(url).text
     try:
@@ -34,5 +44,27 @@ def scrape_symbol(ticker):
             content = content + ticker + '\n'
         f.write(content)
         f.close()
+
+def compile_file(tickers):
+    date = datetime.now().strftime('%Y-%m-%d')
+    data = []
+    updated_prices = []
+    path = 'Data/Watchlist/Temp/'
+    for csv_file in [f for f in listdir(path) if isfile(join(path, f))]:
+        filename = path + csv_file
+        data.append(pandas.read_csv(filename, delimiter=', ', engine='python'))
+    for ticker in tickers:
+        try:
+            updated_prices.append(float(get_ticker_price(ticker)))
+        except:
+            updated_prices.append(-1.0)
+    shutil.rmtree(path, ignore_errors=True)
+    df = pandas.DataFrame(columns=data[0].keys())
+    for d in data:
+        df = df.append(d.iloc[0])
+    df['Symbol'] = tickers
+    df[date] = updated_prices
+    df.to_csv('Data/Watchlist/' + date + '.csv', index=False)
+
 if __name__ == '__main__':
     main()
