e3fc212ba9336277eb3910c7448f676c3b44fd4e
Phillip Sloan <phillipsloan24@gmail.com>
2019-04-02 10:23:32 -0400

    Add build scripts

---
+++ build_exes.cmd
@@ -0,0 +1,8 @@
+pyinstaller -F -w open_MACD_charts.py
+pyinstaller -F -w open_robinhood_stocks.py
+pyinstaller -F -w open_watchlist.py
+pyinstaller -F scrape_exchange.py
+pyinstaller -F -w scrape_MACD_crossover.py
+pyinstaller -F scrape_symbol_list.py
+pyinstaller -F update_crossover_prices.py
+pyinstaller -F update_watchlist_prices.py
---
+++ build_exes.ps1
@@ -0,0 +1,8 @@
+pyinstaller.exe -F -w .\open_MACD_charts.py
+pyinstaller.exe -F -w .\open_robinhood_stocks.py
+pyinstaller.exe -F -w .\open_watchlist.py
+pyinstaller.exe -F .\scrape_exchange.py
+pyinstaller.exe -F -w .\scrape_MACD_crossover.py
+pyinstaller.exe -F .\scrape_symbol_list.py
+pyinstaller.exe -F .\update_crossover_prices.py
+pyinstaller.exe -F .\update_watchlist_prices.py
