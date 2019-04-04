1a7a4aeba299fd0fc474b4ad0a72e30041ebb8cc
Phillip Sloan <phillipsloan24@gmail.com>
2019-04-04 12:12:25 -0400

    Update building
    One line implementation

--- build_exes.cmd
+++ build_exes.cmd
@@ -1,14 +1,1 @@
-pyinstaller -F analyze_MACD.py
-pyinstaller -F analyze_watchlist.py
-pyinstaller -F find_ema_support.py
-pyinstaller -F -w open_MACD_charts.py
-pyinstaller -F -w open_robinhood_stocks.py
-pyinstaller -F -w open_watchlist.py
-pyinstaller -F scrape_exchange.py
-pyinstaller -F -w scrape_MACD_crossover.py
-pyinstaller -F scrape_symbol_list.py
-pyinstaller -F stock_bot.py
-pyinstaller -F update_crossover_prices.py
-pyinstaller -F update_watchlist_prices.py
-pyinstaller -F -w run/run_MACD.py
-pyinstaller -F -w run/run_watchlist.py
+pyinstaller -F analyze_MACD.py && pyinstaller -F analyze_watchlist.py && pyinstaller -F find_ema_support.py && pyinstaller -F -w open_MACD_charts.py && pyinstaller -F -w open_robinhood_stocks.py && pyinstaller -F -w open_watchlist.py && pyinstaller -F scrape_exchange.py && pyinstaller -F -w scrape_MACD_crossover.py && pyinstaller -F scrape_symbol_list.py && pyinstaller -F stock_bot.py && pyinstaller -F update_crossover_prices.py && pyinstaller -F update_watchlist_prices.py && pyinstaller -F -w run\run_MACD.py && pyinstaller -F -w run\run_watchlist.py
--- build_exes.ps1
+++ build_exes.ps1
@@ -1,14 +1,1 @@
-pyinstaller.exe -F ./analyze_MACD.py
-pyinstaller.exe -F ./analyze_watchlist.py
-pyinstaller.exe -F ./find_ema_support.py
-pyinstaller.exe -F -w open_MACD_charts.py
-pyinstaller.exe -F -w open_robinhood_stocks.py
-pyinstaller.exe -F -w open_watchlist.py
-pyinstaller.exe -F ./scrape_exchange.py
-pyinstaller.exe -F -w scrape_MACD_crossover.py
-pyinstaller.exe -F ./scrape_symbol_list.py
-pyinstaller.exe -F ./stock_bot.py
-pyinstaller.exe -F ./update_crossover_prices.py
-pyinstaller.exe -F ./update_watchlist_prices.py
-pyinstaller.exe -F -w run/run_MACD.py
-pyinstaller.exe -F -w run/run_watchlist.py
+pyinstaller.exe -F ./analyze_MACD.py; pyinstaller.exe -F ./analyze_watchlist.py; pyinstaller.exe -F ./find_ema_support.py; pyinstaller.exe -F -w open_MACD_charts.py; pyinstaller.exe -F -w open_robinhood_stocks.py; pyinstaller.exe -F -w open_watchlist.py; pyinstaller.exe -F ./scrape_exchange.py; pyinstaller.exe -F -w scrape_MACD_crossover.py; pyinstaller.exe -F ./scrape_symbol_list.py; pyinstaller.exe -F ./stock_bot.py; pyinstaller.exe -F ./update_crossover_prices.py; pyinstaller.exe -F ./update_watchlist_prices.py; pyinstaller.exe -F -w run/run_MACD.py; pyinstaller.exe -F -w run/run_watchlist.py
