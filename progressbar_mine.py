53d01355cc1a2f8b334a086b199c2a7826b62dff
Phillip Sloan <phillipsloan24@gmail.com>
2019-04-10 08:57:26 -0400

    Fix progress_bar_mine

--- progressbar_mine.py
+++ progressbar_mine.py
@@ -1,19 +1,20 @@
 from progressbar import ProgressBar, Bar, Percentage, ETA, FileTransferSpeed
 
 class progress_bar_mine:
-    widgets = [Bar(marker='=',left='[',right=']')]
-    progress_bar = ProgressBar(widgets=widgets, maxval=100)
+    progress_bar = ProgressBar(widgets=[Bar(marker='=',left='[',right=']')], maxval=100)
     
-    def __init__(self, max_val, widgets=widgets, percentage=True, eta=True, transfer_speed=True):
+    def __init__(self, max_val, widgets=None, percentage=True, eta=True, transfer_speed=True):
+        if widgets == None:
+            widgets = [Bar(marker='=',left='[',right=']')]
         if percentage:
-            self.widgets.append(' ')
-            self.widgets.append(Percentage())
+            widgets.append(' ')
+            widgets.append(Percentage())
         if eta:
-            self.widgets.append(' ')
-            self.widgets.append(ETA())
+            widgets.append(' ')
+            widgets.append(ETA())
         if transfer_speed:
-            self.widgets.append(' ')
-            self.widgets.append(FileTransferSpeed())
+            widgets.append(' ')
+            widgets.append(FileTransferSpeed())
         self.progress_bar = ProgressBar(widgets=widgets, maxval=max_val)
     def start(self, label=None):
         if label:
