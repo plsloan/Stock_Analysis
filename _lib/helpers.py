62f600ffbff6d5febdd09f88b0c59b007abc08f6
Phillip Sloan <phillipsloan24@gmail.com>
2019-04-12 14:09:50 -0400

    Add helpers file

---
+++ _lib/helpers.py
@@ -0,0 +1,25 @@
+import os
+import glob
+import pandas
+import datetime
+
+
+def find_file(starting_path, filename):
+    file_list = []
+    for root, dirs, files in os.walk(starting_path):
+        if filename in files:
+            file_list.append(os.path.join(root, filename))
+    return file_list
+
+def get_directories(path):
+    list_dir = os.listdir(path)
+    temp_dir = []
+    for directory in list_dir:
+        if '.' in directory:
+            temp_dir.append(directory)
+    for directory in temp_dir:
+        list_dir.remove(directory)
+    return list_dir
+
+
+print(find_file('../Data/Watchlist/', 'analyze.txt'))
