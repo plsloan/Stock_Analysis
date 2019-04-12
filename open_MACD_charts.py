733c5a9ade2d3090844d4f4945b48892cf9114d7
Phillip Sloan <phillipsloan24@gmail.com>
2019-04-12 14:10:01 -0400

    Safeguard

--- open_MACD_charts.py
+++ open_MACD_charts.py
@@ -1,8 +1,14 @@
+import pandas
+from datetime import datetime
 import webbrowser as browser
 import scrape_MACD_crossover
 
-def main():
-    df = scrape_MACD_crossover.main()
+def main(already_scraped=False):
+    if not already_scraped:
+        df = scrape_MACD_crossover.main(outside_use=True)
+    else:
+        today = datetime.now().strftime('%Y-%m-%d')
+        df = pandas.read_csv('Data/MACD_Crossover/' + today + '.csv')
     symbols = df['Symbol']
     symbols_under25 = df[df['Close'] <= 25]['Symbol']
     for ticker in symbols_under25:
