2cb340d276479335d0cddde3dd5bc08e2a74a016
Phillip Sloan <phillipsloan24@gmail.com>
2019-04-17 15:52:01 -0400

    Update

--- stock_bot.py
+++ stock_bot.py
@@ -35,12 +35,14 @@ def main():
 
     while datetime.time(datetime.datetime.now().hour, datetime.datetime.now().minute) < datetime.time(16, 5) and datetime.time(datetime.datetime.now().hour, datetime.datetime.now().minute) > datetime.time(7, 0):
         if datetime.datetime.now().hour in [15] and datetime.datetime.now().minute in [15]:
+            print('\nUpdating MACD crossover list...')
             update_crossover_prices.main(all=True)
-        hour = datetime.datetime.now().hour
-        minute = datetime.datetime.now().minute
-        today = datetime.datetime.now().strftime('%Y-%m-%d')
-        filename = datetime.datetime.now().strftime('%Y-%m-%d_%H%M') + '.csv'
-        if hour in [10, 12, 14, 16] and minute in [0]:
+        now = datetime.datetime.now()
+        hour = now.hour
+        minute = now.minute
+        today = now.strftime('%Y-%m-%d')
+        filename = now.strftime('%Y-%m-%d_%H%M') + '.csv'
+        if hour in [9, 10, 12, 14, 16] and minute in [0]:
             filename = 'Data/Watchlist/Day0/' + today + '/' + filename
             if not os.path.exists(filename):
                 print('\nUpdating ' + datetime.datetime.now().strftime('%H:%M') + '...')
