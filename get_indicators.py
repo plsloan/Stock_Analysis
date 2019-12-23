c56ab8bb0d6ae000c12fc39c29865a49d68089a3
plsloan <phillipsloan24@gmail.com>
2019-12-23 07:01:42 -0500

    Update get_indicators

--- get_indicators.py
+++ get_indicators.py
@@ -1,15 +1,24 @@
 import numpy
 
-def getSMA(data, span):
-    return data['Close'].mean()
-def getSMA_set(data, span):
-    return data['Close'].rolling(span, min_periods=1).mean()
-def getEMA(data, span):
-    return data['Close'].ewm(span=span, min_periods=0, adjust=False, ignore_na=True).mean().iloc[-1]
-def getEMA_set(data, span):
-    return data['Close'].ewm(span=span, min_periods=0, adjust=False, ignore_na=True).mean()
+
+def getSMA(data, span, get_last=False):
+    if get_last:
+        return data['Close'].mean()
+    else:
+        return data['Close'].rolling(span, min_periods=1).mean()
+
+
+def getEMA(data, span, get_last=False):
+    if get_last:
+        return data['Close'].ewm(span=span, min_periods=0, adjust=False, ignore_na=True).mean().iloc[-1]
+    else:
+        return data['Close'].ewm(span=span, min_periods=0, adjust=False, ignore_na=True).mean()
+
+
 def getMomentum(data, span):
     return data['Close'].iloc[-1]/data['Close'].iloc[-(span+1)] - 1
+
+
 def getRSI(data):
     series = data['Close']
     period = 14
@@ -19,14 +28,19 @@ def getRSI(data):
         d = u.copy()
         u[delta > 0] = delta[delta > 0]
         d[delta < 0] = -delta[delta < 0]
-        u[u.index[period-1]] = numpy.mean( u[:period] ) #first value is sum of avg gains
+        # first value is sum of avg gains
+        u[u.index[period-1]] = numpy.mean(u[:period])
         u = u.drop(u.index[:(period-1)])
-        d[d.index[period-1]] = numpy.mean( d[:period] ) #first value is sum of avg losses
+        # first value is sum of avg losses
+        d[d.index[period-1]] = numpy.mean(d[:period])
         d = d.drop(d.index[:(period-1)])
-        rs = u.ewm(com=period-1, adjust=False).mean() / d.ewm(com=period-1, adjust=False).mean()
+        rs = u.ewm(com=period-1, adjust=False).mean() / \
+            d.ewm(com=period-1, adjust=False).mean()
         return (100 - 100 / (1 + rs)).iloc[-1]
-    else: 
+    else:
         return 100
+
+
 def getBollingerBand(data):
     w = 20  # span
     x = 2   # standard deviation periods
@@ -35,11 +49,17 @@ def getBollingerBand(data):
     upper = data_bollinger.mean() + data_bollinger.std() * x
     lower = data_bollinger.mean() - data_bollinger.std() * x
     return upper, center, lower
+
+
 def getMACD(data):
-    macd = getEMA_set(data, 12) - getEMA_set(data, 26)
-    sig  = macd.ewm(span=9, min_periods=0, adjust=False, ignore_na=True).mean()
+    macd = getEMA(data, 12) - getEMA(data, 26)
+    sig = macd.ewm(span=9, min_periods=0, adjust=False, ignore_na=True).mean()
     return macd, sig
+
+
 def get_daily_returns(data):
     return (data/data.shift(1)) - 1
+
+
 def get_cumulative_returns(data):
     return (data.iloc[-1]/data.iloc[0]) - 1
