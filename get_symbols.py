9584bdc6c173816557d307b935de8c206481e7eb
plsloan <phillipsloan24@gmail.com>
2020-12-09 22:30:22 -0500

    New formatting

--- get_symbols.py
+++ get_symbols.py
@@ -1,4 +1,4 @@
-import pandas 
+import pandas
 import webbrowser as browser
 import robin_stocks as robinhood
 from pandas_datareader import data as web
@@ -7,13 +7,16 @@ from progressbar import ProgressBar, Bar, Percentage, ETA, FileTransferSpeed
 
 from get_indicators import getEMA, getRSI
 
+
 def getRobinhoodSymbols():
-    login = robinhood.login('phillipsloan24@gmail.com','54bR&Srkm7EU')
+    login = robinhood.login('phillipsloan24@gmail.com', '54bR&Srkm7EU')
     my_stocks = robinhood.build_holdings()
     return list(my_stocks.keys())
+
+
 def printRobinhoodSymbols():
     print('\n----------------------------------- Robinhood Symbols -----------------------------------')
-    login = robinhood.login('phillipsloan24@gmail.com','54bR&Srkm7EU')
+    login = robinhood.login('phillipsloan24@gmail.com', '54bR&Srkm7EU')
     my_stocks = robinhood.build_holdings()
     for ticker in my_stocks.keys():
         print(ticker)
--- open_robinhood_stocks.py
+++ open_robinhood_stocks.py
@@ -1,10 +1,12 @@
 import webbrowser as browser
 from get_symbols import getRobinhoodSymbols
 
+
 def main():
     my_stocks = getRobinhoodSymbols()
     for ticker in my_stocks:
         browser.open('https://finance.yahoo.com/chart/' + ticker)
+
 
 if __name__ == '__main__':
     main()
